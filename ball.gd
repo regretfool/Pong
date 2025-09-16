1e1c8d453fff404e86de1764301bee14b2cc62bc
regretfool <krzeminska.laura@gmail.com>
2025-09-16 17:18:22 +0100

    Added wall & paddle collision, ball bounce

--- ball.gd
+++ ball.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
-var speed = 3
-var direction = Vector2(randf_range(-1,1), randf_range(-1,1))
+var speed: int = 350
+# Random direction vector for shooting ball at the beginning.
+var direction: Vector2 = Vector2(randf_range(-1,1), randf_range(-1,1))
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,7 +9,14 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("action"):
 		velocity = direction * speed
-	move_and_collide(velocity)
+	# If a collision occurs, a KinematicCollision2D object is returned (otherwise, the return is null).
+	var collision: KinematicCollision2D = move_and_collide(velocity * delta)
+	if collision:
+		# Returns the colliding body's shape's normal at the point of collision.
+		velocity = velocity.bounce(collision.get_normal())
+		# Returns the colliding body's attached Object (Wall-StaticBody2D).
+		# We can get properties and methods from it (like detecting hits(signal), name, etc)
+		print_debug("I collided with ", collision.get_collider().name)
